4f462bd5978d692c324ac256b89669d8bda9be42
heini <melme900@gmail.com>
2021-05-13 17:39:30 +0900

    board controller connection

--- routers/board/index.js
+++ routers/board/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./board.controller');
 
-router.use('/', controller.board);
+router.get('/write',controller.write);
+router.get('/view',controller.view);
+router.get('/modify',controller.modify);
+router.get('/', controller.board);
 
 module.exports = router;
